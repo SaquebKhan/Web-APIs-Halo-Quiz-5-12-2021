6d35498850a75f728e3184c7bc80f47571e4e89d
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-05-13 00:02:38 -0400

    update

---
+++ scripts/hsLogic.js
@@ -0,0 +1,8 @@
+// function that retieves the highscore from local storage
+// add display onto page
+
+function highscore(){}
+
+
+// declair your pring highscore function
+highscore();
---
+++ scripts/questions.js
@@ -0,0 +1,58 @@
+var questionBank = [
+    {   
+        question: "How many halo rings are there?",
+        selection: ["3", "7", "10", "14"],
+        answer: "7"
+    },
+    {   
+        question: "What is the purpoues of the rings?",
+        selection: ["Wipe out all sentient life in the Milky Way", "Open a portal to salvation", "Destory planits", "Opens portal to difrent parts of the galaxy"],
+        answer: "Wipe out all sentient life in the Milky Way"
+    },
+    {   
+        question: "Which weponse can you not pick up in Halo 1?", 
+        selection: ["<script>", "<js>", "<javascript>", "<JS>"],
+        answer: "<script>"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        selection: ["if i = 5", "if (i == 5)", "if i = 5 then", "if i == 5 then"],
+        answer: "if (i == 5) "
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        selection: ["<script name='./xxx.js'>", "<script href='./xxx.js'>", "<script scr='./xxx.js'>", "<script ='./xxx.js'>"],
+        answer: "<script scr='./xxx.js'>"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        selection: ["'This is a comment", "//This is a comment", "<!--This is a comment-->", "<!**This is a comment**>"],
+        answer: "//This is a comment"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        selection: ["Math.max(x, y)", "ceil(x, y)", "top(x, y)", "Math.ceil(x, y) "],
+        answer: "Math.max(x, y)"
+    },
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        selection: ["if (i != 5)", "if (i <> 5)", "if i =! 5 then", "if i <> 5"],
+        answer: "if (i != 5)"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        selection: ["function = myFunction()", "function:myFunction()", "function myFunction()", "$Function()"],
+        answer: "function myFunction()"
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        selection: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+        answer: "Math.round(7.25)"
+    },
+    {
+        question: "JavaScript is the same as Java?",
+        selection: ["True", "False", "Maybe", "I don't know"],
+        answer: "False"
+    }
+
+];
