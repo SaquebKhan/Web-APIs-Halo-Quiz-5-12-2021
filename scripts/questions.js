c1f94c5bfd7c283b6c8d445430309751677cd892
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-05-13 14:24:20 -0400

    update

--- scripts/questions.js
+++ scripts/questions.js
@@ -11,48 +11,18 @@ var questionBank = [
     },
     {   
         question: "Which weponse can you not pick up in Halo 1?", 
-        selection: ["<script>", "<js>", "<javascript>", "<JS>"],
-        answer: "<script>"
+        selection: ["Plasma rifle", "Needler", "<Plasma grenade>", "<Fuel rod Gun>"],
+        answer: "<Fuel rod Gun>"
     },
     {
-        question: "How to write an IF statement in JavaScript?",
-        selection: ["if i = 5", "if (i == 5)", "if i = 5 then", "if i == 5 then"],
-        answer: "if (i == 5) "
+        question: "What is Master Chief's name",
+        selection: ["Carter-A259", "Emile-A239", "John-117", "Tom-B292"],
+        answer: "John-117"
     },
     {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        question: "When did the first Halo game come out?",
         selection: ["<script name='./xxx.js'>", "<script href='./xxx.js'>", "<script scr='./xxx.js'>", "<script ='./xxx.js'>"],
         answer: "<script scr='./xxx.js'>"
-    },
-    {
-        question: "How can you add a comment in a JavaScript?",
-        selection: ["'This is a comment", "//This is a comment", "<!--This is a comment-->", "<!**This is a comment**>"],
-        answer: "//This is a comment"
-    },
-    {
-        question: "How do you find the number with the highest value of x and y?",
-        selection: ["Math.max(x, y)", "ceil(x, y)", "top(x, y)", "Math.ceil(x, y) "],
-        answer: "Math.max(x, y)"
-    },
-    {
-        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-        selection: ["if (i != 5)", "if (i <> 5)", "if i =! 5 then", "if i <> 5"],
-        answer: "if (i != 5)"
-    },
-    {
-        question: "How do you create a function in JavaScript?",
-        selection: ["function = myFunction()", "function:myFunction()", "function myFunction()", "$Function()"],
-        answer: "function myFunction()"
-    },
-    {
-        question: "How do you round the number 7.25, to the nearest integer?",
-        selection: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
-        answer: "Math.round(7.25)"
-    },
-    {
-        question: "JavaScript is the same as Java?",
-        selection: ["True", "False", "Maybe", "I don't know"],
-        answer: "False"
     }
 
 ];
