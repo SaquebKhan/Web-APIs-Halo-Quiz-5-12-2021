694060b6ee9c4539d527c84c531a379bbe840b38
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-05-16 03:31:55 -0400

    Update

---
+++ CodingQuiz-starter/assets/js/questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "How many halo rings are there?",
+    choices: ["3", "7", "10", "14"],
+    answer: "7"
+  },
+  {
+    title: "What is the purpoues of the rings?",
+    choices: ["Wipes out all sentient life in the Milky Way", "Open a portal to salvation", "Destory planets", "Opens portal to difrent parts of the galaxy"],
+    answer: "Wipes out all sentient life in the Milky Way"
+  },
+  {
+    title: "Which weponse can you not pick up in Halo 1?",
+    choices: ["Plasma rifle", "Needler", "Plasma grenade", "Energy sword"],
+    answer: "Energy sword"
+  },
+  {
+    title: "What is Master Chief's name",
+    choices: ["Carter-A259", "Emile-A239", "John-117", "Tom-B292"],
+    answer: "John-117"
+  },
+  {
+    title: "When did the first Halo game come out?",
+    choices: ["December 27, 2002", "October 15, 2003", "November 15, 2001", "September 15, 2000"],
+    answer: "November 15, 2001"
+  }
+];
--- scripts/hsLogic.js
+++
@@ -1,8 +0,0 @@
-// function that retieves the highscore from local storage
-// add display onto page
-
-function highscore(){}
-
-
-// declair your pring highscore function
-highscore();
